7407cd40f64e81404f518b125ebd636b8aaaa3e1
miron <mironova-k@kiszi.ru>
2021-08-29 17:36:51 +0300

    Start homework_5

--- src/test/java/com/epam/tc/hw5/cucumber/steps/AbstractStep.java
+++ src/test/java/com/epam/tc/hw5/cucumber/steps/AbstractStep.java
@@ -2,7 +2,6 @@ package com.epam.tc.hw5.cucumber.steps;
 
 import com.epam.tc.hw5.pages.EpamInformationFrameworkPage;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
 
 public abstract class AbstractStep {
 
