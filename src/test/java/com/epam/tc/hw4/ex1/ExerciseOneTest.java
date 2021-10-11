bad6eff5879befe59e2f7a14cdbdc5fab89a3721
KMironova <61177737+KMironova@users.noreply.github.com>
2021-10-11 14:39:26 +0300

    remove unwanted files

--- src/test/java/com/epam/tc/hw4/ex1/ExerciseOneTest.java
+++ src/test/java/com/epam/tc/hw4/ex1/ExerciseOneTest.java
@@ -14,7 +14,7 @@ public class ExerciseOneTest extends TestBase {
     @Test
     @Description("test function: login, correct display icons and text")
     @Feature("base function")
-    @Story("perform login and correct page load")
+    @Story("perfom login and correct page load")
     public void testExerciseOne() {
         EpamInformationFrameworkPage epamInformationFrameworkPage = new EpamInformationFrameworkPage(webDriver, softAssertions);
 
