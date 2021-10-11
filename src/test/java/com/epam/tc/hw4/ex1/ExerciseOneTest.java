8e7e4a38e1a484f48eb62b0eafb257b0617c1be8
KMironova <61177737+KMironova@users.noreply.github.com>
2021-10-11 15:01:23 +0300

    fix file

--- src/test/java/com/epam/tc/hw4/ex1/ExerciseOneTest.java
+++ src/test/java/com/epam/tc/hw4/ex1/ExerciseOneTest.java
@@ -14,7 +14,8 @@ public class ExerciseOneTest extends TestBase {
     @Test
     @Description("test function: login, correct display icons and text")
     @Feature("base function")
-    @Story("perfom login and correct page load")
+    @Story("perform login and correct page load")
+
     public void testExerciseOne() {
         EpamInformationFrameworkPage epamInformationFrameworkPage = new EpamInformationFrameworkPage(webDriver, softAssertions);
 
